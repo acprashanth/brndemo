7caba0841a1db15878c3949ba8cc4e40510cdf1f
Prashanth <Prashanth@prashanths-macbook-pro.local>
2017-06-10 17:03:19 +0530

    committing to DEV branch

--- demo10June/ViewController.swift
+++ demo10June/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("hello")
+        print("I'm in DEV branch")
     }
 
     override func didReceiveMemoryWarning() {
